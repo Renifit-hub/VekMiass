80fac4923ee8de811db3895e20e9db3a84b0722e
renifit-copilot <testmail09099@gmail.com>
2025-12-17 20:39:39 +0500

    first commit

---
+++ themes/vekmiass/patterns/hidden-404.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Title: 404
+ * Slug: vek-miass/404
+ * Inserter: no
+ *
+ * @package vek-miass
+ * @since 1.0.0
+ */
+?>
+<!-- wp:heading {"textAlign":"center", "level": 1} -->
+<h1 class="wp-block-heading has-text-align-center">
+	<?php esc_html_e( 'Page not found', 'vek-miass' ); ?>
+</h1>
+<!-- /wp:heading -->
+<!-- wp:paragraph -->
+<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'vek-miass' ); ?></p>
+<!-- /wp:paragraph -->
+<!-- wp:search {"label":"<?php echo esc_html_x( 'Search', 'Search form label', 'vek-miass' ); ?>","showLabel":false,"buttonText":"<?php echo esc_html_x( 'Search', 'Search form submit button text', 'vek-miass' ); ?>"} /-->
